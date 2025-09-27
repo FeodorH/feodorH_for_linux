797b785ca5e26a26067b02168c63d1cef142ebfc
FeodorH <fholkin@yandex.ru>
2025-09-27 15:03:52 +0300

    number 2

--- main.cpp
+++ main.cpp
@@ -6,13 +6,23 @@ int main() {
   /// Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-
-    std::cout << "$ ";
+    
     std::string input;
 
-    std::cin>>input;
+    while (true) {
+        std::cout << "$ ";
+        
+        if (!std::getline(std::cin, input)) {
+            // Ctrl+D
+            std::cout << "\nExit" << std::endl;
+            break;
+        }
+        
+        std::cout<<input<<" :)\n";
+
+    }
 
     std::cout<<input<<" :)\n";
-    
+
     return 0;
 }
