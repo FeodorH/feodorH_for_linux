1f5dda274fbd7b882150a8a8da80e5d991ace3e2
FeodorH <fholkin@yandex.ru>
2025-09-27 15:10:49 +0300

    number 4

--- main.cpp
+++ main.cpp
@@ -6,23 +6,32 @@ int main() {
   /// Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-    
-    std::string input;
+
+    std::ofstream history_file("/home/main_user/kubsh_history.txt", std::ios::app);
+
+    if (!history_file.is_open()) {
+        std::cerr << "Error: Cannot open history file!" << std::endl;
+        return 1;
+    }
+
+    history_file << std::unitbuf;//for rebuffing
 
     while (true) {
         std::cout << "$ ";
+        std::string input;
         
-        if ( !std::getline(std::cin, input) || (input == "\\q")) {
+        if (!std::getline(std::cin, input)||(input == "\\q")) {//exit 1
             // Ctrl+D
             std::cout << "\nExit" << std::endl;
             break;
         }
+
+        history_file<<input<<"\n";
         
         std::cout<<input<<" :)\n";
 
     }
 
-    std::cout<<input<<" :)\n";
-
+    history_file.close();
     return 0;
 }
