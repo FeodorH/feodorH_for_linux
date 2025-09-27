162ede3f026806e34989c04b8970229b669170f1
FeodorH <fholkin@yandex.ru>
2025-09-27 14:57:57 +0300

    number 1

--- main.cpp
+++ main.cpp
@@ -1,30 +1,18 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 int main() {
   /// Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    while (true) {
-        std::cout << "$ ";
-        std::string input;
-        
-        if (!std::getline(std::cin, input)) {//exit 1
-            // Ctrl+D
-            std::cout << "\nExit" << std::endl;
-            break;
-        }
-        
-        if (input == "\\q") {//exit 2
-            break;
-        }
+    std::cout << "$ ";
+    std::string input;
 
-        int a = 0;
-        
-        std::cout<<input<<"\n";
-        //std::cout << input << ": command not found" << std::endl;
-    }
-    //new code
+    std::cin>>input;
+
+    std::cout<<input<<" :)\n";
+    
     return 0;
 }
